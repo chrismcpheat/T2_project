1da4d25780c5ee1442e6c206aa15e9e029181261
chrismcpheat <B00339658@studentmail.uws.ac.uk>
2018-03-22 16:35:11 +0000

    incorporated GUI functionality in 'client' program
    Signed-off-by: chrismcpheat <B00339658@studentmail.uws.ac.uk>

--- client.py
+++ client.py
@@ -1,39 +1,94 @@
+# https://www.youtube.com/watch?v=gjU3Lx8XMS8
+# https://stackoverflow.com/questions/3352918/how-to-center-a-window-on-the-screen-in-tkinter
+
 import socket
 import json
 from tcp_settings import IP, PORT
 import tkinter
 from tkinter import *
 from functools import partial
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import time
+import numpy as np
 
 
-class ClientGUI():
-    def __init__(self, master):
-        # upper frame and its contents
-        upperFrame = Frame(master, width=50, height=100)
-        upperFrame.grid(row=0, column=0, padx=10, pady=2)
 
-        # Right Frame and its contents
-        lowerFrame = Frame(master, width=50, height=60)
-        lowerFrame.grid(row=1, column=0, padx=10, pady=2)
+class ClientGUI(tkinter.Frame):
 
-        # button frame
-        btnFrame = Frame(upperFrame, width=50, height=30)
-        btnFrame.grid(row=1, column=0, padx=10, pady=2)
+    # test function for dropdown menu
+    def doNothing(self):
+        print("TESTING")
 
-        # text box
-        self.windowLog = Text(lowerFrame, width=50, height=30, takefocus=0)
-        self.windowLog.grid(row=2, column=0, padx=10, pady=2)
+    def __init__(self, master=None):
+        tkinter.Frame.__init__(self, master)
+        self.starttime = (time.time())
+        self.CreateWidgets()
 
-        # pressing a button can trigger a function call (and we can pass input arguments / parameters)
-        # https://pythonprogramming.net/passing-functions-parameters-tkinter-using-lambda/
-        tempBtn = Button(btnFrame, text="Temp", command=partial(self.GetTemp))
-        tempBtn.grid(row=0, column=0, padx=40, pady=2)
 
-        loadBtn = Button(btnFrame, text="Load", command=partial(self.GetLoad))
-        loadBtn.grid(row=0, column=1, padx=40, pady=2)
+    def CreateWidgets(self):
 
-        clockBtn = Button(btnFrame, text="Clock", command=partial(self.GetClock))
-        clockBtn.grid(row=0, column=2, padx=40, pady=2)
+        # set title and icon of application
+        self.winfo_toplevel().title("ScanWare")
+        self.winfo_toplevel().iconbitmap(r'c:\t2\icon.ico')
+
+        # dropdown menu bar
+        menuBar = Menu(master=root)
+        master=root.config(menu=menuBar)
+
+        # creates left frame for labels and buttons
+        leftFrame = Frame(master=root, width=50, height=50)
+        leftFrame.grid(row=0, column=0, padx=10, pady=2)
+
+        # creates right frame for graph
+        rightFrame = Frame(master=root, width=50, height=50)
+        rightFrame.grid(row=0, column=1, padx=10, pady=2)
+
+        # 'File' menu
+        fileMenu = Menu(menuBar, tearoff=0)
+        menuBar.add_cascade(label="File", menu=fileMenu)
+        fileMenu.add_command(label="New 2", command=partial(self.doNothing))
+        fileMenu.add_command(label="New", command=partial(self.doNothing))
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", command=root.destroy)
+
+        # 'Edit' menu
+        editMenu = Menu(menuBar, tearoff=0)
+        menuBar.add_cascade(label="Edit", menu=editMenu)
+        editMenu.add_command(label="Redo", command=partial(self.doNothing))
+
+        # --- labels
+        # cpu
+        label_cpu = Label(leftFrame, text="CPU")
+        label_cpu.grid(row=0, column=0, padx=10, pady=10, sticky=W)
+        # clocks
+        label_clock = Label(leftFrame, text="Clocks")
+        label_clock.grid(row=3, column=0, padx=10, pady=10, sticky=W)
+
+        # --- buttons and commands
+        # temperature
+        self.temp_button = tkinter.Button(leftFrame, text="Temperature", width=15, command=lambda: self.plot(canvas,ax))
+        self.temp_button.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+        # load time
+        # self.load_button = tkinter.Button(leftFrame, text="Load Time", width=15, command=lambda: self.plot(canvas, ax))
+        # self.load_button.grid(row=2, column=0, padx=10, pady=5, sticky=W)
+        self.load_button = tkinter.Button(leftFrame, text="Load Time", width=15, command=self.GetLoad)
+        self.load_button.grid(row=2, column=0, padx=10, pady=5, sticky=W)
+        # clock speed
+        self.clock_button = tkinter.Button(leftFrame, text="Clock Speed", width=15, command=lambda: self.plot(canvas, ax))
+        self.clock_button.grid(row=4, column=0, padx=10, pady=5, sticky=W)
+        # reset
+        self.reset_button = tkinter.Button(master=root, text="Reset", width=15, command=lambda: self.reset(canvas, ax))
+        self.reset_button.grid(row=2, column=1, pady=20)
+
+        # create canvas and graph in right frame
+        mpl.rc('axes', edgecolor='b')
+        fig=plt.figure(figsize=(8,6))
+        ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=False)
+        canvas = FigureCanvasTkAgg(fig, rightFrame)
+        canvas.get_tk_widget().grid(row=0,column=1)
+        canvas.draw()
 
     def GetTemp(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -58,22 +113,30 @@ class ClientGUI():
     def GetLoad(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             # Connect to server and send data
-            print("connecting again...")
+            #print("connecting again...")
             sock.connect((IP, PORT))
 
             # request cpu load data from the server
             request = {"type": "request",
                        "param": "cpu_core_load"}
-            print(f"client sent: {request}")
+            #print(f"client sent: {request}")
             sock.sendall(bytes(json.dumps(request), "utf-8"))
-            print("load request sent...")
+            #print("load request sent...")
 
             # Receive load data from the server
             load_response = str(sock.recv(2048), "utf-8")
             print(f"server load received: {load_response}")
+            load_time = json.loads(load_response)
+            core1 = load_time['CPU Core #1']
+            core2 = load_time['CPU Core #2']
+            print(core1)
+            print(core2)
 
+            """
             self.windowLog.insert(0.0, load_response)
             self.windowLog.insert(0.0, "\n")
+            """
+
 
     def GetClock(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -95,11 +158,23 @@ class ClientGUI():
             self.windowLog.insert(0.0, load_response)
             self.windowLog.insert(0.0, "\n")
 
+    def plot(self, canvas, ax):
+        x = epoch_time = (time.time())
+        x = x - self.starttime
+        y = np.random.random()
+        ax.plot(x,y, color='red', marker='o', linestyle='dashed', linewidth=2, markersize=6)
+        canvas.draw()
+
+    def reset(self, canvas, ax):
+        ax.clear()
+        self.starttime = (time.time())
+        canvas.draw()
+
 
 if __name__ == '__main__':
     # tkinter gui
     # create root
-    root = tkinter.Tk()
-    client = ClientGUI(root)
-    root.mainloop()
+    root = Tk()
+    client = ClientGUI(master=root)
+    client.mainloop()
     print("exiting...")
