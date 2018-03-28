315e255b8703850118b448d66520d741d8591c6e
chrismcpheat <B00339658@studentmail.uws.ac.uk>
2018-03-28 23:24:47 +0100

    added legends & axes changed colours and styles also added an ignore for matplotlib warnings
    Signed-off-by: chrismcpheat <B00339658@studentmail.uws.ac.uk>

--- client.py
+++ client.py
@@ -10,10 +10,16 @@ from functools import partial
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.patches as mpatches
 import time
 import numpy as np
+import warnings
+import matplotlib.cbook
+warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 
 
+# last pushed button
+# changes state to value of button, to prevent data being reset/cleared if the button pushed is the same as the last
 def enum(**named_values):
     return type('Enum', (), named_values)
 LastPush = enum(TEMP='temp', LOAD='load', CLOCK='clock', POWER='power')
@@ -28,27 +34,31 @@ class ClientGUI(tkinter.Frame):
 
     def __init__(self, master=None):
         tkinter.Frame.__init__(self, master)
+        master.configure(background='grey')
         self.starttime = (time.time())
         self.CreateWidgets()
 
 
     def CreateWidgets(self):
 
-        # set title and icon of application
+        # ------ set title and icon of application
         self.winfo_toplevel().title("ScanWare")
         self.winfo_toplevel().iconbitmap(r'c:\t2\icon.ico')
 
+        # ------ frames
+        # creates left frame for labels and buttons
+        leftFrame = Frame(master=root, width=150, height=300, bg="#EEF1F6")
+        leftFrame.grid(row=0, column=0, padx=10, pady=0)
+        leftFrame.grid_propagate(0)
+
+        # creates right frame for graph
+        middleFrame = Frame(master=root, width=0, height=50)
+        middleFrame.grid(row=0, column=1, pady=2)
+
+        # ------ menus
         # dropdown menu bar
         menuBar = Menu(master=root)
         master=root.config(menu=menuBar)
-
-        # creates left frame for labels and buttons
-        leftFrame = Frame(master=root, width=50, height=50)
-        leftFrame.grid(row=0, column=0, padx=10, pady=2)
-
-        # creates right frame for graph
-        rightFrame = Frame(master=root, width=50, height=50)
-        rightFrame.grid(row=0, column=1, padx=10, pady=2)
 
         # 'File' menu
         fileMenu = Menu(menuBar, tearoff=0)
@@ -61,7 +71,7 @@ class ClientGUI(tkinter.Frame):
         # 'Edit' menu
         editMenu = Menu(menuBar, tearoff=0)
         menuBar.add_cascade(label="Edit", menu=editMenu)
-        editMenu.add_command(label="Redo", command=partial(self.doNothing))
+        editMenu.add_command(label="Reset", command=lambda: self.reset())
 
         # ------ labels
         # cpu
@@ -71,12 +81,13 @@ class ClientGUI(tkinter.Frame):
         label_clock = Label(leftFrame, text="Clocks", font="Helvetica 10 bold")
         label_clock.grid(row=4, column=0, padx=10, pady=10, sticky=W)
 
+
         # ------ buttons and commands
         # cpu temperatures
         self.temp_button = tkinter.Button(leftFrame, text="Temperatures", width=15, command=self.GetTemp)
         self.temp_button.grid(row=1, column=0, padx=10, pady=5, sticky=W)
-        # cpu load times
-        self.load_button = tkinter.Button(leftFrame, text="Load Times", width=15, command=self.GetLoad)
+        # cpu loads
+        self.load_button = tkinter.Button(leftFrame, text="Loads", width=15, command=self.GetLoad)
         self.load_button.grid(row=2, column=0, padx=10, pady=5, sticky=W)
         # cpu powers`
         self.power_button = tkinter.Button(leftFrame, text="Powers", width=15, command=self.GetPower)
@@ -89,20 +100,17 @@ class ClientGUI(tkinter.Frame):
         self.reset_button.grid(row=2, column=1, pady=20)
 
         # create canvas and graph in right frame
-        mpl.rc('axes', edgecolor='b')
-        fig=plt.figure(figsize=(8,6))
-        """
-        self.ax0 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.ax3 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        """
-        self.ax0 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.ax3 = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
-        self.canvas = FigureCanvasTkAgg(fig, rightFrame)
-        self.canvas.get_tk_widget().grid(row=0,column=1)
+        self.fig=plt.figure(figsize=(8,5), facecolor="#EEF1F6")
+
+
+
+        self.ax0 = self.fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
+        self.ax1 = self.fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
+        self.ax2 = self.fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
+        self.ax3 = self.fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=False)
+
+        self.canvas = FigureCanvasTkAgg(self.fig, middleFrame)
+        self.canvas.get_tk_widget().grid(row=0, column=1, padx=0)
         self.canvas.draw()
 
         # ------ empty lists for cpu temperature, cpu loads, clock speeds and cpu powers for X and Y axes
@@ -141,6 +149,7 @@ class ClientGUI(tkinter.Frame):
 
 
     def GetTemp(self):
+
         if self.prev_button_state != LastPush.TEMP:
             self.reset()
             self.prev_button_state = LastPush.TEMP
@@ -170,6 +179,22 @@ class ClientGUI(tkinter.Frame):
             self.temp_c1_x.append(x)
             self.temp_c1_y.append(y1)
             self.temp_c2_y.append(y2)
+
+            label_x = 'Time (s)'
+            label_y = 'Temperature (°C)'
+            legend_0 = mpatches.Patch(color='blue', label='CPU Core #1')
+            legend_1 = mpatches.Patch(color='red', label='CPU Core #2')
+
+            plt.legend(handles=[legend_0, legend_1])
+            plt.xlabel(label_x, fontsize=8, labelpad=10)
+            plt.ylabel(label_y, fontsize=7, labelpad=10)
+
+            """
+            # ------ attempting to place legend outside - not working
+            legend_0 = mpatches.Patch(color='red', label='CPU Core #1')
+            legend_1 = mpatches.Patch(color='blue', label='CPU Core #2')
+            plt.legend(handles=[legend_0, legend_1], bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+            """
 
             self.plot(self.temp_c1_x, self.temp_c1_y, 0)
             self.plot(self.temp_c1_x, self.temp_c2_y, 1)
@@ -205,6 +230,15 @@ class ClientGUI(tkinter.Frame):
             self.load_c1_x.append(x)
             self.load_c1_y.append(y1)
             self.load_c2_y.append(y2)
+
+            label_x = 'Time (s)'
+            label_y = 'Load (%)'
+            legend_0 = mpatches.Patch(color='blue', label='CPU Core #1')
+            legend_1 = mpatches.Patch(color='red', label='CPU Core #2')
+
+            plt.legend(handles=[legend_0, legend_1])
+            plt.xlabel(label_x, fontsize=8, labelpad=10)
+            plt.ylabel(label_y, fontsize=8, labelpad=10)
 
             self.plot(self.load_c1_x, self.load_c1_y, 0)
             self.plot(self.load_c1_x, self.load_c2_y, 1)
@@ -248,6 +282,18 @@ class ClientGUI(tkinter.Frame):
             self.power_cc_y.append(y3)
             self.power_cg_y.append(y4)
 
+            label_x = 'Time (s)'
+            label_y = 'Power (W)'
+            legend_0 = mpatches.Patch(color='blue', label='DRAM')
+            legend_1 = mpatches.Patch(color='red', label='Package')
+            legend_2 = mpatches.Patch(color='green', label='Cores')
+            legend_3 = mpatches.Patch(color='yellow', label='Graphics')
+
+            plt.legend(handles=[legend_0, legend_1, legend_2, legend_3])
+            plt.xlabel(label_x, fontsize=8, labelpad=10)
+            plt.ylabel(label_y, fontsize=8, labelpad=10)
+
+
             self.plot(self.power_cd_x, self.power_cd_y, 0)
             self.plot(self.power_cd_x, self.power_cp_y, 1)
             self.plot(self.power_cd_x, self.power_cc_y, 2)
@@ -289,20 +335,31 @@ class ClientGUI(tkinter.Frame):
             self.clock_c2_y.append(y2)
             self.clock_bs_y.append(y3)
 
+            label_x = 'Time (s)'
+            label_y = 'Rate (MHz)'
+            legend_0 = mpatches.Patch(color='blue', label='CPU Core #1')
+            legend_1 = mpatches.Patch(color='red', label='CPU Core #2')
+            legend_2 = mpatches.Patch(color='green', label='Bus Speed')
+
+            plt.legend(handles=[legend_0, legend_1, legend_2])
+            plt.xlabel(label_x, fontsize=8, labelpad=10)
+            plt.ylabel(label_y, fontsize=8, labelpad=10)
+
             self.plot(self.clock_c1_x, self.clock_c1_y, 0)
             self.plot(self.clock_c1_x, self.clock_c2_y, 1)
             self.plot(self.clock_c1_x, self.clock_bs_y, 2)
 
 
     def plot(self, x, y, id):
+
         if id == 0:
-            self.ax0.plot(x, y, color='blue', marker='o', linestyle='dashed', linewidth=1, markersize=3)
+            self.ax0.plot(x, y, color='#2124ff', marker='o', linestyle='solid', linewidth=1, markersize=2)
         elif id == 1:
-            self.ax1.plot(x, y, color='red', marker='o', linestyle='dashed', linewidth=1, markersize=3)
+            self.ax1.plot(x, y, color='#ff2121', marker='o', linestyle='solid', linewidth=1, markersize=2)
         elif id == 2:
-            self.ax2.plot(x, y, color='green', marker='o', linestyle='dashed', linewidth=1, markersize=3)
+            self.ax2.plot(x, y, color='#21ff21', marker='o', linestyle='solid', linewidth=1, markersize=2)
         elif id == 3:
-            self.ax3.plot(x, y, color='black', marker='o', linestyle='dashed', linewidth=1, markersize=3)
+            self.ax3.plot(x, y, color='#fff721', marker='o', linestyle='solid', linewidth=1, markersize=2)
         self.canvas.draw()
 
 
@@ -342,6 +399,7 @@ if __name__ == '__main__':
     # tkinter gui
     # create root
     root = Tk()
+    # root.configure(background='#EEF1F6')
     client = ClientGUI(master=root)
     client.mainloop()
     print("exiting...")
