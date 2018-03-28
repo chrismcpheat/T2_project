b0607a7fbbd34969a8bd4aa6643c058e125d419d
chrismcpheat <B00339658@studentmail.uws.ac.uk>
2018-03-28 23:22:09 +0100

    rearranged order of functions
    Signed-off-by: chrismcpheat <B00339658@studentmail.uws.ac.uk>

--- OHM.py
+++ OHM.py
@@ -8,6 +8,7 @@ class OHM:
     def __init__(self):
         self.hwmon = wmi.WMI(namespace="root\OpenHardwareMonitor")
 
+
     def get_core_temps(self):
         data = {}
         sensors_temp = self.hwmon.Sensor(["Name", "Parent", "Value", "Identifier", "SensorType", "Max"],
@@ -32,6 +33,7 @@ class OHM:
 
         return data
 
+
     def get_core_loads(self):
         data = {}
         sensors_load = self.hwmon.Sensor(SensorType="Load")
@@ -51,24 +53,6 @@ class OHM:
 
         return data
 
-    def get_clock_speeds(self):
-        data = {}
-        sensors_speed = self.hwmon.Sensor(SensorType="Clock")
-
-        if debug:
-            print(sensors_speed)
-
-        for speed in sensors_speed:
-            if (speed.Identifier.find("ram") == -1) and (speed.Identifier.find("hdd") == -1) and (
-                    speed.Name.find("Total") == -1):
-
-                if debug:
-                    print(f"{speed.value}, {speed.name}")
-
-                data['type'] = "clock speeds"
-                data[speed.name] = speed.value
-
-        return data
 
     def get_core_powers(self):
         data = {}
@@ -89,6 +73,27 @@ class OHM:
 
         return data
 
+
+    def get_clock_speeds(self):
+        data = {}
+        sensors_speed = self.hwmon.Sensor(SensorType="Clock")
+
+        if debug:
+            print(sensors_speed)
+
+        for speed in sensors_speed:
+            if (speed.Identifier.find("ram") == -1) and (speed.Identifier.find("hdd") == -1) and (
+                    speed.Name.find("Total") == -1):
+
+                if debug:
+                    print(f"{speed.value}, {speed.name}")
+
+                data['type'] = "clock speeds"
+                data[speed.name] = speed.value
+
+        return data
+
+
 if __name__ == '__main__':
 
     my_ohm = OHM()
@@ -96,7 +101,7 @@ if __name__ == '__main__':
     print(core_temps)
     core_loads = my_ohm.get_core_loads()
     print(core_loads)
-    clock_speeds = my_ohm.get_clock_speeds()
-    print(clock_speeds)
     cpu_powers = my_ohm.get_core_powers()
     print(cpu_powers)
+    clock_speeds = my_ohm.get_clock_speeds()
+    print(clock_speeds)
