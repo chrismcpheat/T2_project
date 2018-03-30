1c83f108053f48f329c4747c93f82dccce3bf1e9
chrismcpheat <B00339658@studentmail.uws.ac.uk>
2018-03-30 21:15:13 +0100

    server program now opens OpenHardWare monitor application
    Signed-off-by: chrismcpheat <B00339658@studentmail.uws.ac.uk>

--- server.py
+++ server.py
@@ -1,31 +1,27 @@
 import socketserver
 import time
 from tcp_settings import IP, PORT
-
 from OHM import OHM
 import json
+import os
 
+os.startfile('OpenHardwareMonitor\OpenHardwareMonitor.exe')
 
+# request handler class
 class TCPRequestHandler(socketserver.BaseRequestHandler):
     """
     The request handler receives the response data from the client
     """
 
     def __init__(self, request, client_address, server):
-
-        print("initialising. . . ")
         # Init the base class
         super(TCPRequestHandler, self).__init__(request, client_address, server)
 
     def handle(self):
         # time.sleep(1)
-        """
-        The request handler class.
-        """
-        # print("client data incoming...")
+
         #  self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip().decode("utf-8")  # NB bytes to string
-        # print(f"data received: {self.data}")
 
         # request OHM data
         requested_data = {}
@@ -41,18 +37,15 @@ class TCPRequestHandler(socketserver.BaseRequestHandler):
             elif request['param'] == "cpu_core_power":
                 requested_data = my_ohm.get_core_powers()
 
-        # print(requested_data)
         # return the data to the client
         response = json.dumps(requested_data)
         self.request.sendall(response.encode('utf-8'))
 
-
 def main():
     with socketserver.TCPServer((IP, PORT), TCPRequestHandler) as server:
-        print("starting server")
+        print("Server Started")
         server.serve_forever()
-
 
 if __name__ == '__main__':
     main()
-    print("exiting...")
+    print("Server Stopped")
